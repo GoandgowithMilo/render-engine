e84a4181f450a19c4dab153c9d0e335898bfa479
James Milo <james.milo@rezdy.com>
2019-12-23 21:50:05 +1100

    We're rendering with pixels now!!!

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class DemoViewer {
@@ -42,50 +43,62 @@ public class DemoViewer {
                 tris.add(new Triangle(new Vertex(-100, 100, -100),
                         new Vertex(100, -100, -100),
                         new Vertex(-100, -100, 100),
-                        Color.BLUE));
+                        Color.YELLOW));
 
-                g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.YELLOW);
 
-                // rotation
+                // rotations
                 double heading = Math.toRadians(headingSlider.getValue());
-                Matrix3 transform = new Matrix3(new double[] {
-                    Math.cos(heading), 0, -Math.sin(heading),
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                    Math.cos(heading), 0, Math.sin(heading),
                     0, 1, 0,
-                    Math.sin(heading), 0, Math.cos(heading)
+                    -Math.sin(heading), 0, Math.cos(heading)
                 });
 
-//                // Attempt at implementing rotation
-//                double heading2 = Math.toRadians(pitchSlider.getValue());
-//                Matrix3 transform2 = new Matrix3(new double[] {
-//                    Math.cos(heading2), 0, -Math.sin(heading2),
-//                    0, 1, 0,
-//                    Math.sin(heading2), 0, Math.cos(heading2)
-//                });
-//
-//                for(Triangle t : tris) {
-//                    Vertex v1 = transform2.transform(t.v1);
-//                    Vertex v2 = transform2.transform(t.v2);
-//                    Vertex v3 = transform2.transform(t.v3);
-//                    Path2D path = new Path2D.Double();
-//                    path.moveTo(v1.x, v1.y);
-//                    path.lineTo(v2.x, v2.y);
-//                    path.lineTo(v3.x, v2.y);
-//                    path.closePath();
-//                    g2.draw(path);
-//                }
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                    1, 0, 0,
+                    0, Math.cos(pitch), Math.sin(pitch),
+                    0, -Math.sin(pitch), Math.cos(pitch)
+                });
+
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
                 for(Triangle t : tris) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
                     Vertex v3 = transform.transform(t.v3);
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.closePath();
-                    g2.draw(path);
+
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
+
+                    // compute rectangular bounds for the triangle
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for(int y = minY; y <= maxY; y++) {
+                        for(int x = minX; x <= maxX; x++) {
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                img.setRGB(x, y, t.color.getRGB());
+                            }
+                        }
+                    }
                 }
+                g2.drawImage(img, 0, 0, null);
             }
         };
         headingSlider.addChangeListener(e -> renderPanel.repaint());
